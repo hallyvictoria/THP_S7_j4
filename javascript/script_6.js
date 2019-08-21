902f174be77307c34e4ffd3863c01744f1950213
Carsano <damien.fabiano@gadz.org>
2019-08-21 16:16:35 +0200

    script 5

---
+++ javascript/script_6.js
@@ -0,0 +1,10 @@
+function transformARN(arn) {
+	let memo = [
+	{acideAmine: 'Sérine', codons:['UCU', 'UCC', 'UCA', 'AGU', 'AGC']},
+	{acideAmine: 'Proline', codons:['CCU', 'CCC', 'CCA', 'CCG']},
+	{acideAmine: 'Leucine', codons:['UUA', 'UUG']},
+	{acideAmine: 'Phénylalanine', codons:['UUU', 'UUC']},
+	{acideAmine: 'Arginine', codons: ['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG']},
+	{acideAmine: 'Tyrosine', codons:['UAU', 'UAC']}
+	]
+}

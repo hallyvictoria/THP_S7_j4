602278f6813531cae6b32bb18d8185ee49840ec9
Carsano <damien.fabiano@gadz.org>
2019-08-22 15:49:32 +0200

    update arn

--- javascript/script_6.js
+++ javascript/script_6.js
@@ -7,12 +7,9 @@ function transformARN(arn) {
 	{acideAmine: 'Arginine',      codons:['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG']},
 	{acideAmine: 'Tyrosine',      codons:['UAU', 'UAC']							   }
 	]
-	firstCodon  = arn[0] + arn[1] + arn[2]
-	secondCodon = arn[3] + arn[4] + arn[5]
-	thirdCodon  = arn[6] + arn[7] + arn[8]
-	fourthCodon = arn[9] + arn[10] + arn[11]
+	allCodons = arn.match(/.{1,3}/g);
+
 	output = []
-	allCodons = [firstCodon, secondCodon, thirdCodon, fourthCodon]
 	allCodons.forEach(function(codon) {
 		memo.forEach(function(translation) {
 			if (translation.codons.includes(codon)){
@@ -22,5 +19,5 @@ function transformARN(arn) {
 	})
 	console.log(output.join('-'))
 }
-transformARN('CCGUCGUUGCGCUACAGC') // # => Proline-Sérine-Leucine-Arginine 
-transformARN('CCUCGCCGGUACUUCUCG') // # => Proline-Arginine-Arginine-Tyrosine
+transformARN('CCGUCGUUGCGCUACAGC') // # => Proline-Sérine-Leucine-Arginine-Tyrosine-Sérine
+transformARN('CCUCGCCGGUACUUCUCG') // # => Proline-Arginine-Arginine-Tyrosine-Phénylalanine-Sérine
